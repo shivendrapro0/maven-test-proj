c479f8d0c6e9791e19df73a23379112595a58f79
Shivendra <shivendrapro0@gmail.com>
2018-04-06 09:38:29 +0000

    first commit

---
+++ java-app/src/main/java/shiv/org/app/App.java
@@ -0,0 +1,13 @@
+package shiv.org.app;
+
+/**
+ * TestApp
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Basic Java Application!" );
+    }
+}
---
+++ web-app/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Test Web-App!</h2>
+</body>
+</html>
